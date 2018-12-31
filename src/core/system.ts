14a296ac8fe5854aa76d7b9825a94541f80c2196
CloudRex <cloudrex@outlook.com>
2018-12-30 19:47:01 -0500

    Add action type

--- src/core/system.ts
+++ src/core/system.ts
@@ -5,6 +5,8 @@ export const hiddenPropertySuffix: string = "$$xlib_";
 
 export type PromiseOr<T> = Promise<T> | T;
 
+export type Action<T = void> = () => T;
+
 export default class System {
     public static open(path: string): FileHandle | null {
         if (!fs.existsSync(path)) {
